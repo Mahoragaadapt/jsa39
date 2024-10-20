2d98f447f4f42eceea37de6e027d9f7f798f957d
Mahoragaadapt <ilovesuffer271@gmail.com>
2024-10-20 20:25:52 +0700

    Add files via upload

---
+++ b2/java1.js
@@ -0,0 +1,22 @@
+//ghi đè giá trị biến a //
+let a = 3 ;
+console.log("giá trị a trc khi ghi đè",a);
+a =4;
+console.log("giá trị biến a :",a);
+
+//mảng lưu nhiều giá trị(nhiều phần tử) tại 1 thời điểm//
+let colorList = ["yellow","green","red"]
+
+//in ra phần tử số 2 (0,1,"2")//
+console.log("phần tử có chỉ số là 2",colorList[2]);
+
+//đặt tên biến và gắn giá trị//
+let colorListlength=colorList.length;
+console.log("số lượng phần tử của colorList",colorListlength);
+
+//gọi tên phần tử cuối cùng//
+console.log(colorList[colorListlength-1]);
+
+let random = ["white"]
+random[0]
+
